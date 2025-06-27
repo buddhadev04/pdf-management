4cb21691431ec1a4edf7eaca7043a3258714ad8f
buddhadev04 <buddhadevdas237@gmail.com>
2025-06-28 01:44:35 +0530

    hot fix

--- controllers/approvalController.js
+++ controllers/approvalController.js
@@ -13,8 +13,8 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'ankitsingh07897@gmail.com',
-    pass: 'elrj uxrl srbk lgoc',
+    user: 'officialjjunction@gmail.com',
+    pass: 'tpmmwlxlksoeiqlx',
   },
 });
 
